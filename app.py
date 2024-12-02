0652e4bdd6f38198f41ba572690b568f11132f57
sebg22 <sebmp48@gmail.com>
2024-12-02 15:53:47 +0100

    firstcommit

--- app.py
+++ app.py
@@ -111,7 +111,28 @@ def view_customer():
 
     return render_template("view_customer.html", user=user, items=items, restaurants=restaurants)
 
+##############################
+@app.get("/restaurant-items")
+@x.no_cache
+def view_restaurant_items():
+    if not session.get("user", ""):
+        return redirect(url_for("view_login"))
+    user = session.get("user")
+    db, cursor = x.db()
 
+    # i used something called a subquery here, so it is easier to understand for us. otherwise the query is not very easy to read
+    cursor.execute("""
+        SELECT * FROM users WHERE user_pk IN (
+        SELECT user_role_user_fk FROM users_roles
+        JOIN roles ON users_roles.user_role_role_fk = roles.role_pk
+        WHERE roles.role_name = 'restaurant')
+    """)
+    restaurants = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM items ORDER BY item_created_at DESC")
+    items = cursor.fetchall()
+
+    return render_template("view_restaurant_items.html", user=user, items=items, restaurants=restaurants)
 
 
 ##############################
